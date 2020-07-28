74e33d6d2ffac543dbbacbd684995d4d29ddefd5
ziaulsiddiqui <ziauls42@gmail.com>
2020-07-29 00:35:04 +0530

    Added path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ require(`dotenv`).config({
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
+  pathPrefix: "/portfolio-gatsby",
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
